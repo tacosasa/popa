7c9a68b70111696ed8e9f5d75601bc4c679f0183
tacosasa <157867712+tacosasa@users.noreply.github.com>
2024-01-27 14:54:17 +1000

    Create yaro_slave.cs

---
+++ yaro_slave.cs
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    int age = 10;
+string name = "yarra";
+bool isGoal = true;
+Console.WriteLine(name + " "+ age);
+
+if ((age < 18) && (age > 18)) {
+    Console.WriteLine("lox");
+}
+else {
+    Console.WriteLine("no lox");
+
+}
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine("no lox");
+}
+  }
+}
